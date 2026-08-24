d4a19f496343951251352306bc70cb51a4f7ffe4
goyo <gregoriolarronde@gmail.com>
2026-08-24 10:35:43 -0300

    .

---
+++ tp3/punto1/CuentaBanco.java
@@ -0,0 +1,17 @@
+package tp3.punto1;
+
+public class CuentaBanco {
+    private int balance;
+
+    public CuentaBanco(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void retiroBancario(int retiro) {
+        this.balance -= retiro;
+    }
+}
---
+++ tp3/punto1/Main.java
@@ -0,0 +1,13 @@
+package tp3.punto1;
+
+public class Main {
+    public static void main(String[] args) {
+        CuentaBanco cb = new CuentaBanco(50);
+        VerificarCuenta vcL = new VerificarCuenta(cb);
+        VerificarCuenta vcJ = new VerificarCuenta(cb);
+        Thread lucas = new Thread(vcL, "Lucas");
+        Thread jere = new Thread(vcJ, "Jere");
+        lucas.start();
+        jere.start();
+    }
+}
